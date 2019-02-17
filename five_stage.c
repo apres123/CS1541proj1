d826d5967c2d5d5e5817fe4674397f4078363fcf
Andrew Preston <arp119@pitt.edu>
2019-02-17 17:49:03 -0500

    Puts command line arguments into correct order (trace_file_name, prediction_method, trace_view_on)

--- five_stage.c
+++ five_stage.c
@@ -34,7 +34,10 @@ int main(int argc, char **argv)
   trace_file_name = argv[1];
   if (argc >= 3) trace_view_on = atoi(argv[2]) ;
   
-  if (argc == 4) prediction_method = atoi(argv[3]) ;
+  if (argc == 4) {
+	  prediction_method = atoi(argv[2]);
+	  trace_view_on = atoi(argv[3]);
+  }
 
   fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
 
